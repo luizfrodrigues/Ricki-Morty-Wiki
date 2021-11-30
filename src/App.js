ede2ec14566fbfcdeb731b872e08c61d6f6e9a0c
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-11-30 13:01:29 -0300

    :construction: Pagination component

--- src/App.js
+++ src/App.js
@@ -48,7 +48,11 @@ function App() {
         </div>
       </div>
 
-      <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber} />
+      <Pagination
+        info={info}
+        pageNumber={pageNumber}
+        setPageNumber={setPageNumber}
+      />
     </div>
   );
 }
