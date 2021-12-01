abcfd9989d9d8b3946ccdd6278891f2fb9a78b4e
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-12-01 13:53:03 -0300

    :construction: Filter component

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Gender from '../Filters/Category/Gender';
+import Species from '../Filters/Category/Species';
+import Status from '../Filters/Category/Status';
 
 const Filters = () => {
-  return <div>Filters</div>;
+  return (
+    <div className='col-3'>
+      <div className='text-center fw-bold fs-4 mb-2'>Filter</div>
+      <div
+        style={{ cursor: 'pointer' }}
+        className='text-center text-primary text-decoration-underline mb-4'>
+        Clear Filters
+      </div>
+
+      <div className='accordion' id='accordionExample'>
+        <Status />
+        <Species />
+        <Gender />
+      </div>
+    </div>
+  );
 };
 
 export default Filters;
